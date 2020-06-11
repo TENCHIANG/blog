a1d2c8dee04c2f4c2a6e586058691c59222ee02b
TENCHIANG <yy5209zz@gmail.com>
2020-06-11 18:03:27 +0800

    绕过游戏检测

--- Linux/mem.c
+++ Linux/mem.c
@@ -48,6 +48,16 @@ int getPid (char *pkg) {
 }
 
 /**
+ * 绕过游戏监控
+ */
+int bypass () {
+    int code = system("echo 0 > /proc/sys/fs/inotify/max_user_watches") % 255;
+    if (code != 0)
+        fprintf(stderr, "绕过游戏监控失败 状态码 %d\n", code);
+    return code;
+}
+
+/**
  * 获取mem文件句柄 失败返回 -1
  */
 int openProcMem (int pid) {
@@ -56,11 +66,11 @@ int openProcMem (int pid) {
     return open(buf, O_RDWR, O_SYNC);
 }
 
-// gcc -Wall -O3 /sdcard/Pictures/MemoryTools/mem.c -o /data/local/tmp/test/mem
-// su -c '/data/local/tmp/test/mem -p com.tencent.lycqsh -a 82bb400e -o 3210'
+// tsu -c 'gcc -Wall -O3 /sdcard/Pictures/mem.c -o /data/local/tmp/test/mem'
+// tsu -c '/data/local/tmp/test/mem -p com.tencent.lycqsh -a 82bb400e -o 3210'
 int main (int argc, char **argv) {
     unsigned long addr = 0;
-    int count = 8; // 字节数
+    int count = 4; // 字节数
     int offset = 0; // addr偏移
     char *pkg = 0;
     
@@ -99,9 +109,11 @@ int main (int argc, char **argv) {
         exit(1);
     }
     
+    bypass();
+    
 	int fd = openProcMem(pid);
     if (pid == -1) {
-        fprintf(stderr, "fd = %d\n", fd);
+        fprintf(stderr, "fd = %d pid = %d\n", fd, pid);
         exit(1);
     }
     
@@ -110,19 +122,24 @@ int main (int argc, char **argv) {
     //long data = 100;
     //res = pwrite64(fd, &data, sizeof(data), addr);
     
-	double value = 0;
+	/*double value = 0;
 	res = pread64(fd, &value, count, addr);
     if (res == -1) {
-        fprintf(stderr, "读取内存 %#lx 失败\n", addr);
+        fprintf(stderr, "读取内存 %#lx 失败 pid = %d\n", addr, pid);
         exit(1);
     }
+	printf("%#lx %ld %#lx %g\n", addr, value, value, value);*/
     
-    char buf[BUFSIZE] = { 0 };
-    res = pread64(fd, &buf, count, addr);
-    buf[count] = 0;
-    printf("buf [%s] %d %d %d\n", buf, strlen(buf), res);
-    
-	printf("%#lx %ld %#lx %lf\n", addr, value, value, value);
+    unsigned char buf[BUFSIZE] = { 0 };
+    res = pread64(fd, buf, count, addr);
+    if (res == -1) {
+        fprintf(stderr, "读取内存 %#lx 失败 pid = %d\n", addr, pid);
+        exit(1);
+    }
+    printf("%#lx %ld %#lx %g ", addr, *(long *)buf, *(long *)buf, *(double *)buf);
+    for (int i = 0; i < count; i++)
+        printf("%02x ", buf[i]);
+    printf("\n");
     
     res = close(fd);
     if (res == -1) {
